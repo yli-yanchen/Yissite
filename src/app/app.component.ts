ba94fc088df8a216c5219fcfaacd4bd892650faa
Cheryl Lee <yli.yanchen@gmail.com>
2024-09-14 14:06:04 -0700

    new components test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <!-- <img
+          class="brand-logo"
+          src="@/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        /> -->
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  // templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'yissite';
+  title = 'home';
 }
